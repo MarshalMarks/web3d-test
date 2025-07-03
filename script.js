6bc813debce2a0807d8e3a3ffe0e82fb4c8a7a69
Marshal Marks <marshmarks@gmail.com>
2025-07-03 01:21:51 -0500

    added shadows and changed button cosmetics

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-document.querySelector('.btn').addEventListener('click', () => {
-    document.querySelector('#table')
-        .classList.toggle('change')
-    document.querySelector('#letter-1')
-        .classList.toggle('change')
+const button = document.querySelector('.btn');
+const table = document.querySelector('#table');
+const letter = document.querySelector('#letter');
+const shadow = document.querySelector('#letter-shadow');
+const arrow = document.querySelector('#button-arrow');
+
+var isAtTable = false;
+
+button.addEventListener('click', () => {
+    table.classList.toggle('change')
+    letter.classList.toggle('change')
+    shadow.classList.toggle('change')
+    arrow.classList.toggle('change')
+    isAtTable = !isAtTable;
+    if(isAtTable) {
+        button.textContent = "Press to step back";
+    } else {
+        button.textContent = "Press to step closer";
+    }
 ;})
