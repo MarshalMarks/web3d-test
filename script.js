b25a98962b582f4da8a2a07872a596dbc66ed566
Marshal Marks <marshmarks@gmail.com>
2025-07-03 00:04:23 -0500

    added interaction and readme

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.btn').addEventListener('click', () => {
+    document.querySelector('#table')
+        .classList.toggle('change')
+    document.querySelector('#letter-1')
+        .classList.toggle('change')
+;})
